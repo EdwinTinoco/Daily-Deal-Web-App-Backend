a85dc438f791bfc77d08ed771384f17855242d8e
EdwinTinoco <jaredlotic@gmail.com>
2020-10-28 01:18:47 -0600

    updated the pickup msg in the email when user purchase

--- app.py
+++ app.py
@@ -288,12 +288,12 @@ def fulfill_order(session):
          print(pickup)
       
          msg = Message('Kudu -- Pick the product up to the store --', recipients=[session['metadata']['customerEmail']])
-         msg.body = f'''You need to pick the product up in the store {pickup['pickup_name']}
-                     The address is:
-                     {pickup['pickup_line_1']} {pickup['pickup_line_2']}
-                     {pickup['pickup_city']}, {pickup['pickup_state']} 
-                     {pickup['pickup_country']}, {pickup['pickup_zip_code']}
-                     '''
+         msg.body = f'''You need to pick up the product in the following address:
+            {pickup['pickup_name']}            
+            {pickup['pickup_line_1']} {pickup['pickup_line_2']}
+            {pickup['pickup_city']}, {pickup['pickup_state']} 
+            {pickup['pickup_country']}, {pickup['pickup_zip_code']}
+            '''
          mail.send(msg)   
    except Exception as e:
       error_message = "There was an issue with the email to send the pickup address. Contact support services."
